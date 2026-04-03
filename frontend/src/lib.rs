6ef1137d20c6996fe1eb8ccc489523673706f07e
corust-ci <ci@corust.ai>
2026-04-03 17:32:26 +0000

    feat: Add frontend unit/component tests

---
+++ frontend/src/lib.rs
@@ -0,0 +1,5 @@
+pub mod api;
+pub mod app;
+pub mod components;
+pub mod models;
+pub mod router;
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,11 +1,5 @@
 use leptos::*;
 
-pub mod api;
-pub mod app;
-pub mod components;
-pub mod models;
-pub mod router;
-
 fn main() {
-    mount_to_body(app::TodoApp);
+    mount_to_body(frontend::app::TodoApp);
 }
